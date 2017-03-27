a54144efbc8f270b76737e605416aa3edc24cf74
Zeno <zenocbda@gmail.com>
2017-03-27 16:06:05 +0800

    beautify the code

--- pixiv.py
+++ pixiv.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import re
 import getpass
 from io import BytesIO
 from PIL import Image
 
 
-sess = requests.session()
 class Pixiv:
     def __init__(self, pixiv_id: str, password: str):
         self.base_url = "https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index"
@@ -34,8 +32,7 @@ class Pixiv:
             "password": self.passwd,
             "post_key": self.post_key
         }
-        tmp = self.sess.post(self.login_url, data=login_data)
-        self.cookie = tmp.headers.get('Set-cookie')
+        self.sess.post(self.login_url, data=login_data)
         #至此模拟登录完成
         #这里的self.cookie没有用到
 
@@ -47,7 +44,7 @@ class Pixiv:
             temp_cookie += x
 
         #便于测试,从指定某个画师作品的首页获取
-        painter_page = self.sess.get("http://www.pixiv.net/member_illust.php?id=465133", headers={"Cookie": temp_cookie})
+        painter_page = self.sess.get("http://www.pixiv.net/member_illust.php?id=465133")
         fav_soup = BeautifulSoup(painter_page.text, 'lxml')
 
         #从这个画师的作品中获取含多个图片的画册,关键词multiple
@@ -60,30 +57,26 @@ class Pixiv:
 
         #对所有的画册深度搜索,下载里面的图片,(0,13)是手动查出来的
         for i in range(0, 13):
-            work_lib = self.sess.get("http://www.pixiv.net/"+self.work_list[i])
-            tmp_soup = BeautifulSoup(work_lib.text, 'lxml')
-            tmp_find = tmp_soup.find_all('div', 'img-container')
-            tmp_find_soup = BeautifulSoup(str(tmp_find), 'lxml')
-            print(tmp_find_soup.a['href'])
+            manga_page = "http://www.pixiv.net" + self.work_list[i].replace('medium', 'manga')
+            print(self.work_list[i])
+            work_sub_lib = self.sess.get(manga_page)
 
-            #获取到图片lib
-            work_sub_lib = self.sess.get("http://www.pixiv.net/"+tmp_find_soup.a['href'])
             work_sub_lib_src = BeautifulSoup(work_sub_lib.text, 'lxml')
-            tmp_work_sub_lib_src = work_sub_lib_src.find_all('img', 'image ui-scroll-view')
+            img_src_register = work_sub_lib_src.find_all('img', 'image ui-scroll-view')
 
             #将所有的图片源地址放到sub_list里
             sub_list = []
-            for x in tmp_work_sub_lib_src:
+            for x in img_src_register:
                 tmp = BeautifulSoup(str(x), 'lxml')
                 sub_list.append(tmp.img['data-src'])
 
             #下载图片
             for x in sub_list:
-                self.get_image(x, temp_cookie)
+                self.get_image(x, manga_page)
 
 
-    def get_image(self, url, cookie):
-        img = self.sess.get(url, headers={"Referer": self.base_url, "Cookie": cookie})
+    def get_image(self, url, referer):
+        img = self.sess.get(url, headers={"Referer": referer})
         im = Image.open(BytesIO(img.content))
         im.save("pics/" + url[-26:-4] + ".png", 'png')
 
@@ -92,7 +85,6 @@ if __name__ == "__main__":
     account = input("input account:")
     passwd  = getpass.getpass("input password:")
     tmp = Pixiv(account, passwd)
-    # tmp.login()
-    # tmp.get_image()
+    tmp.login()
     tmp.search_for_painter_by_id()
 
